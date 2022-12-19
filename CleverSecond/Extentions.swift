dce02123c32c86931c8412bc29c53ae42ab34c6e
volchetski <zhenyahd2010@gmail.com>
2022-12-19 22:53:37 +0300

    Added method height of cell

--- CleverSecond/Extentions.swift
+++ CleverSecond/Extentions.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import UIKit
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return namesOfItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.identifier, for: indexPath) as? CustomCell else { return UITableViewCell() }
+        
+            cell.configure(text: namesOfItems[indexPath.row], descriptionText: descriptionsOfItems[indexPath.row], imageName: namesOfItems[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+    
+}
+
