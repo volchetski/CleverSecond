a09a3e239869a3a395eedc36d4d97bec20a39842
volchetski <zhenyahd2010@gmail.com>
2022-12-19 13:17:11 +0300

    Added button and added constraints

---
+++ CleverSecond/Extentions.swift
@@ -0,0 +1,8 @@
+//
+//  Extentions.swift
+//  CleverSecond
+//
+//  Created by Евгений Волчецкий on 16.12.22.
+//
+
+import Foundation
--- CleverSecond/ViewController.swift
+++ CleverSecond/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.dataSource = self
+        table.delegate = self
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
+    
+    private lazy var editButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Edit", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 20)
+        button.tintColor = .label
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        view.backgroundColor = .white
+        
+        addSubviews()
+        tableView.delegate = self
+        tableView.dataSource = self
+        setUpConstraints()
     }
-
-
+    
+    
+    private func addSubviews() {
+        view.addSubview(tableView)
+        view.addSubview(editButton)
+    }
+    
+    private func setUpConstraints() {
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: editButton.topAnchor),
+        
+            editButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
+            editButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -60),
+            editButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            editButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)])
+    }
 }
 
