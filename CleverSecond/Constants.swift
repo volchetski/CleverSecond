f39c9ba9f6009cd5a2e7f0502a97596789c0af20
volchetski <zhenyahd2010@gmail.com>
2022-12-20 21:40:24 +0300

    Added constants

---
+++ CleverSecond/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  CleverSecond
+//
+//  Created by Евгений Волчецкий on 20.12.22.
+//
+
+import Foundation
+
+let cellIdentifier = "CustomTableViewCell"
+let itemName = "Title"
+let itemDescription = "Description"
