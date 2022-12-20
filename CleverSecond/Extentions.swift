c2c56887e573e88cc1485c651ee66267d04d27b3
volchetski <zhenyahd2010@gmail.com>
2022-12-20 21:42:13 +0300

    Added method for move and delete cell

--- CleverSecond/Extentions.swift
+++ CleverSecond/Extentions.swift
@@ -10,13 +10,13 @@ import UIKit
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrayOfImageName.count
+        return arrayWithAllItems.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.identifier, for: indexPath) as? CustomCell else { return UITableViewCell() }
         
-        cell.configure(text: "\(itemName) \(indexPath.row + 1)", descriptionText: "\(itemDescription) \(indexPath.row + 1)", imageName: arrayOfImageName[indexPath.row])
+        cell.configure(text: "\(arrayWithAllItems[indexPath.row].itemName) \(indexPath.row + 1)", descriptionText: "\(arrayWithAllItems[indexPath.row].itemDescription) \(indexPath.row + 1)", imageName: arrayWithAllItems[indexPath.row].imageName)
         
         return cell
     }
@@ -24,14 +24,14 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
     }
-
+    
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         return .delete
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            arrayOfImageName.remove(at: indexPath.row)
+            arrayWithAllItems.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .left)
         }
     }
@@ -41,6 +41,17 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let item = arrayWithAllItems[sourceIndexPath.row]
+        arrayWithAllItems.remove(at: sourceIndexPath.row)
+        arrayWithAllItems.insert(item, at: destinationIndexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let destination = DescriptionController()
+        destination.model = arrayWithAllItems[indexPath.row]
+        
+        navigationController?.pushViewController(destination, animated: true)
+        
     }
     
 }
